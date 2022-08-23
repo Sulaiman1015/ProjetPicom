90acac420ba958bffe8855e1c4c73e2555b89242
Sulaiman1015 <sulaiman1015@gmail.com>
2022-08-23 11:24:21 +0200

    add files in business class

---
+++ ProjetPicomBack/src/main/java/fr/orsys/as/ProjetPicom/business/Tarif.java
@@ -0,0 +1,29 @@
+package fr.orsys.as.ProjetPicom.business;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tarif {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double prixEnEuros;
+    @ManyToOne
+    private Administrateur administrateur;
+    @ManyToOne
+    private TrancheHoraire trancheHoraire;
+}
