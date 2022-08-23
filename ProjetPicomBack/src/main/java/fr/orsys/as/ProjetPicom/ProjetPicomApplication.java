c0f5c5b6cd7142db2c6ed6c222b2d691745e7430
Sulaiman1015 <sulaiman1015@gmail.com>
2022-08-23 10:32:43 +0200

    Initial commit

---
+++ ProjetPicomBack/src/main/java/fr/orsys/as/ProjetPicom/ProjetPicomApplication.java
@@ -0,0 +1,13 @@
+package fr.orsys.as.ProjetPicom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPicomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetPicomApplication.class, args);
+	}
+
+}
---
+++ ProjetPicomBack/src/test/java/fr/orsys/as/ProjetPicom/ProjetPicomApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orsys.as.ProjetPicom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPicomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
